b3a9bc5abe56459977b42b97e7ea52637acf0d50
zorlovskiy <oreol1234yandex.ru@mail.ru>
2023-06-27 18:50:52 +0300

    add sql cmd 27.05

--- cmd_story.sql
+++ cmd_story.sql
@@ -1861,3 +1861,162 @@ SELECT DISTINCT city FROM airports
 SELECT count ( * ) FROM a1
 JOIN ( SELECT DISTINCT city FROM airports ) AS a2 ON a1.city <> a2.city;
 \s sql_cmd_2506_1802.sql
+
+SELECT count( s.seat_no )
+FROM seats s
+JOIN flights_v f
+ON s.aircraft_code = f.aircraft_code
+WHERE s.aircraft_code = f.aircraft_code
+;
+SELECT * FROM tickets;
+SELECT * FROM ticket_flights;
+SELECT * FROM ticket_flights;
+\q
+INSERT INTO aircrafts_tmp
+VALUES ( 'SU9', 'Sukhoi SuperJet', 3000 )
+ON CONFLICT ON CONSTRAINT aircrafts_tmp_pkey
+DO UPDATE SET model = excluded.model,
+range = excluded.range
+RETURNING *;
+\d
+CREATE TEMP TABLE aircrafts_tmp AS
+SELECT * FROM aircrafts WITH NO DATA;
+ALTER TABLE aircrafts_tmp
+ADD PRIMARY KEY ( aircraft_code );
+ALTER TABLE aircrafts_tmp
+ADD UNIQUE ( model );
+CREATE TEMP TABLE aircrafts_log AS
+SELECT * FROM aircrafts WITH NO DATA;
+ALTER TABLE aircrafts_log
+ADD COLUMN when_add timestamp;
+ALTER TABLE aircrafts_log
+ADD COLUMN operation text;
+CREATE TEMP TABLE aircrafts_tmp
+( LIKE aircrafts INCLUDING CONSTRAINTS INCLUDING INDEXES );
+WITH add_row AS
+( INSERT INTO aircrafts_tmp
+SELECT * FROM aircrafts
+RETURNING *
+)
+INSERT INTO aircrafts_log
+SELECT add_row.aircraft_code, add_row.model, add_row.range,
+current_timestamp, 'INSERT'
+FROM add_row;
+SELECT * FROM aircrafts_tmp ORDER BY model;
+WITH add_row AS
+( INSERT INTO aircrafts_tmp
+VALUES ( 'SU9', 'Сухой Суперджет-100', 3000 )
+ON CONFLICT DO NOTHING
+RETURNING *
+)
+INSERT INTO aircrafts_log
+SELECT add_row.aircraft_code, add_row.model, add_row.range,
+current_timestamp, 'INSERT'
+FROM add_row;
+INSERT INTO aircrafts_tmp
+VALUES ( 'SU9', 'Сухой Суперджет', 3000 )
+ON CONFLICT ON CONSTRAINT aircrafts_tmp_pkey
+DO UPDATE SET model = excluded.model,
+range = excluded.range
+RETURNING *;
+COPY aircrafts_tmp FROM '/home/PostgreSQL/aircrafts.txt';
+COPY aircrafts_tmp FROM '/home/workstation/PostgreSQL/aircrafts.sql';
+COPY aircrafts_tmp FROM '/home/workstation/PostgreSQL/aircrafts.txt';
+COPY aircrafts_tmp FROM '/home/workstation/aircrafts.txt';
+COPY aircrafts_tmp TO '/home/workstation/aircrafts.txt'
+WITH ( FORMAT csv );
+WITH update_row AS
+( UPDATE aircrafts_tmp
+SET range = range * 1.2
+WHERE model ~ '^Bom'
+RETURNING *
+)
+INSERT INTO aircrafts_log
+SELECT ur.aircraft_code, ur.model, ur.range,
+current_timestamp, 'UPDATE'
+FROM update_row ur;
+WITH update_row AS
+( UPDATE aircrafts_tmp
+SET range = range * 1.2
+WHERE model ~ '^Бом'
+RETURNING *
+)
+INSERT INTO aircrafts_log
+SELECT ur.aircraft_code, ur.model, ur.range,
+current_timestamp, 'UPDATE'
+FROM update_row ur;
+SELECT * FROM aircrafts_log
+WHERE model ~ '^Bom' ORDER BY when_add;
+SELECT * FROM aircrafts_log
+WHERE model ~ '^Бом' ORDER BY when_add;
+CREATE TEMP TABLE tickets_directions AS
+SELECT DISTINCT departure_city, arrival_city FROM routes;
+ALTER TABLE tickets_directions
+ADD COLUMN last_ticket_time timestamp;
+ALTER TABLE tickets_directions
+ADD COLUMN tickets_num integer DEFAULT 0;
+CREATE TEMP TABLE ticket_flights_tmp AS
+SELECT * FROM ticket_flights WITH NO DATA;
+ALTER TABLE ticket_flights_tmp
+ADD PRIMARY KEY ( ticket_no, flight_id );
+WITH sell_ticket AS
+( INSERT INTO ticket_flights_tmp
+( ticket_no, flight_id, fare_conditions, amount )
+VALUES ( '1234567890123', 30829, 'Economy', 12800 )
+RETURNING *
+)
+UPDATE tickets_directions td
+SET last_ticket_time = current_timestamp,
+tickets_num = tickets_num + 1
+WHERE ( td.departure_city, td.arrival_city ) =
+( SELECT departure_city, arrival_city
+FROM flights_v
+WHERE flight_id = ( SELECT flight_id FROM sell_ticket )
+);
+SELECT *
+FROM tickets_directions
+WHERE tickets_num > 0;
+WITH sell_ticket AS
+( INSERT INTO ticket_flights_tmp
+(ticket_no, flight_id, fare_conditions, amount )
+VALUES ( '1234567890123', 7757, 'Economy', 3400 )
+RETURNING *
+)
+UPDATE tickets_directions td
+SET last_ticket_time = current_timestamp,
+tickets_num = tickets_num + 1
+FROM flights_v f
+WHERE td.departure_city = f.departure_city
+AND td.arrival_city
+= f.arrival_city
+AND f.flight_id = ( SELECT flight_id FROM sell_ticket );
+SELECT *
+FROM tickets_directions
+WHERE tickets_num > 0;
+WITH delete_row AS
+( DELETE FROM aircrafts_tmp
+WHERE model ~ '^Бом'
+RETURNING *
+)
+INSERT INTO aircrafts_log
+SELECT dr.aircraft_code, dr.model, dr.range,
+current_timestamp, 'DELETE'
+FROM delete_row dr;
+SELECT * FROM aircrafts_log
+WHERE model ~ '^Бом'
+ORDER BY when_add;
+WITH min_ranges AS
+( SELECT aircraft_code,
+rank() OVER (
+PARTITION BY left( model, 6 )
+ORDER BY range
+) AS rank
+FROM aircrafts_tmp
+WHERE model ~ '^Аэробус' OR model ~ '^Боинг'
+)
+DELETE FROM aircrafts_tmp a
+USING min_ranges mr
+WHERE a.aircraft_code = mr.aircraft_code
+AND mr.rank = 1
+RETURNING *;
+\s sql_cmd_2706_1435.sql
