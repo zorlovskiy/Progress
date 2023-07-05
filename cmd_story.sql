6f75c3617b4a5b3a6845e59aaf64295fe034ff37
zorlovskiy <oreol1234yandex.ru@mail.ru>
2023-07-05 18:52:43 +0300

    add sql cmd 5.07

--- cmd_story.sql
+++ cmd_story.sql
@@ -2776,3 +2776,53 @@ UPDATE modes2 SET mode = 'HIGH1' WHERE num = 1;
 COMMIT;
 SELECT * FROM modes2 WHERE mode IN ('LOW1', 'HIGH1');
 \s sql_cmd_407.sql
+\q
+BRGIN;
+BEGIN;
+SELECT * FROM aircrafts_tmp WHERE range < 2000;
+DELETE FROM aircrafts_tmp WHERE range < 2000;
+END;
+SELECT * FROM aircrafts_tmp;
+BEGIN;
+SELECT * FROM aircrafts_tmp WHERE range < 2000;
+DELETE FROM aircrafts_tmp WHERE range < 2000;
+END;
+SELECT * FROM aircrafts_tmp;
+BEGIN;
+UPDATE aircrafts_tmp SET range = 6100 WHERE aircraft_code = '320';
+END;
+BEGIN;
+INSERT INTO aircrafts_tmp VALUES (333, 'Helicopter', 6333);
+END;
+BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+UPDATE modes2 SET mode = 'LOW1' WHERE num = 100001;
+COMMIT;
+SELECT * FROM modes2 WHERE mode IN ('LOW1', 'HIGH1');
+\q
+EXPLAIN SELECT * FROM aircrafts;
+EXPLAIN ( COSTS OFF ) SELECT *
+FROM aircrafts;
+EXPLAIN SELECT *
+FROM aircrafts
+WHERE model ~ 'Аэро';
+EXPLAIN SELECT *
+FROM aircrafts
+ORDER BY aircraft_code;
+EXPLAIN SELECT *
+FROM bookings
+ORDER BY book_ref;
+EXPLAIN SELECT *
+FROM bookings
+WHERE book_ref > '0000FF' AND book_ref < '000FFF'
+ORDER BY book_ref;
+EXPLAIN SELECT *
+FROM seats
+WHERE aircraft_code = 'SU9';
+EXPLAIN SELECT book_ref
+FROM bookings
+WHERE book_ref < '000FFF'
+ORDER BY book_ref;
+EXPLAIN SELECT count( * )
+FROM seats
+WHERE aircraft_code = 'SU9';
+\s sql_cmd_507.sql
